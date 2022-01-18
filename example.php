8bbbc4dda6c9e6d5acff67ee501fe2a0dd4902ff
AliRezvaniAra <91538203+AliRezvaniAra@users.noreply.github.com>
2022-01-18 15:23:40 +0330

    Create example.php

---
+++ example.php
@@ -0,0 +1,8 @@
+<?php
+dd(preg_match('/^[^0-9^.][a-zA-Z0-9]+(\.?[a-z]+)*@gmail.com/', 'ali.rezvaniara@gmail.com'));
+?>
+<html>
+  <body>
+    <input type="text" id="email" name="email" pattern="^[^0-9^.][a-zA-Z0-9]+(\.?[a-z]+)*@gmail.com" >
+  </body>
+</html>
